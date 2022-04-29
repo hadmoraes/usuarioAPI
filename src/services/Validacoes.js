9e538973e8b56a0d5bdc78ff381d2609e821a1bf
Hadassa Moraes <96262130+hadmoraes@users.noreply.github.com>
2022-04-29 16:53:10 -0300

    update validacoes and tests

--- src/services/Validacoes.js
+++ src/services/Validacoes.js
@@ -1,3 +1,5 @@
+import moment from "moment";
+
 class Validacoes{
 
     static validaNome(nome){
@@ -13,6 +15,15 @@ class Validacoes{
         const validacaoEmail =  /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         return email.match(validacaoEmail) != null
     }
+
+    static validaData(data){
+        return moment(data, 'DD/MM/YYYY',true).isValid()
+    }
+
+    static validaNumero(numero){
+        return (typeof numero) == "number"
+    }
+
 
 }
 
--- src/test/Validacoes.test.js
+++ src/test/Validacoes.test.js
@@ -1,8 +1,16 @@
 import Validacoes from "../services/Validacoes.js";
 
+/**
+ * Testes para validacao de nome / string
+ */
+
 test("Validar se o nome tem pelo menos 3 caracteres", ()=>{
     expect(Validacoes.validaNome("Mel")).toBe(true)
 });
+
+/**
+ * Testes para validacao de telefone
+ */
 
 test("Valida se o telefone tem 11 digitos", ()=>{
     expect(Validacoes.validaTelefone("12345678901")).toBe(true)
@@ -12,9 +20,14 @@ test("Verifica se o telefone tem menos de 11 digitos", ()=>{
     expect(Validacoes.validaTelefone("123456789")).toBe(false)
 });
 
-test("Verifica se o telefone tem menos de 11 digitos", ()=>{
+test("Verifica se o telefone tem todos os caracteres numeros", ()=>{
     expect(Validacoes.validaTelefone("1234567890a")).toBe(false)
 });
+
+/**
+ * Testes para validacao de email
+ */
+
 
 test("Verifica se o email é valido", ()=>{
     expect(Validacoes.validaEmail('email@email.br')).toBe(true)
@@ -24,3 +37,28 @@ test("Verifica se o email contem @", ()=>{
     expect(Validacoes.validaEmail('emailemail.br')).toBe(false)
 })
 
+/**
+ * Testes para validacao de data
+ */
+
+
+test("Verifica se a data esta no formato correto DD/MM/YYYY", ()=>{
+    expect(Validacoes.validaData('28/05/2020')).toBe(true)
+})
+
+test("Verifica se a data esta no formato trocado YYYY/MM/DD", ()=>{
+    expect(Validacoes.validaData('2020/05/25')).toBe(false)
+})
+
+
+/**
+ * Testes para validacao de numero 
+ */
+
+ test("Verifica se a entrada é um inteiro", ()=>{
+    expect(Validacoes.validaNumero(250)).toBe(true)
+})
+
+test("Verifica se a entrada é um numero com ponto flutuante", ()=>{
+    expect(Validacoes.validaNumero(400.58)).toBe(true)
+})
